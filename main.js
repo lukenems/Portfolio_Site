b27250c4144c97452266eb957795c4a19df48b30
Luke Nemy <lukenemy7@gmail.com>
2019-06-19 13:56:16 -0700

    updated portfolio section with aligned images and desc

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function menuToggle(){
-    console.log(this.innerHTML)
     let menu = document.getElementsByClassName("menu-drop");
     for(i=0; i < menu.length; i++){
         menu[i].classList.toggle('open');
