fe83133b81364a56bcc40182147545fbc34da236
Luke Nemy <lukenemy7@gmail.com>
2019-06-19 13:37:03 -0700

    added onclick atts for nav menu anchors

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function menuToggle(){
+    console.log(this.innerHTML)
     let menu = document.getElementsByClassName("menu-drop");
     for(i=0; i < menu.length; i++){
         menu[i].classList.toggle('open');
