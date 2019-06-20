8df6537618fccf3ac3522009d510beafc12179e0
Luke Nemy <lukenemy7@gmail.com>
2019-06-20 15:10:39 -0700

    added project_hangry site and removed unnessesary images

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-app.listen(port);
+app.listen(port, function(){
+    console.log('Server running at port 3000: http://127.0.0.1:3000');
+});
+ 
+app.get('/', function(request, response){
+    response.sendFile('/index.html');
+});
